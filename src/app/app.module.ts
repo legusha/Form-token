53f7978bd7750e47c9301b60ce33f311fb942fc0
vlad.legusha <vlad.legusha@gmail.com>
2022-07-02 12:35:15 +0300

    added fonts to styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
-import { FormComponent } from './form/form.component';
+import {FormTokenComponent} from './form-token/form-token.component';
+import { FormTitleComponent } from './form-token/form-token-title/form-token-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    FormComponent
+    FormTitleComponent,
+    FormTokenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
